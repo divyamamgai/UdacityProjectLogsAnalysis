9e6e171f9430a9ee170873fd9a8a2c26ab2f9fd1
vagrant <vagrant@vagrant.vm>
2017-09-26 19:16:31 +0000

    Refactored code and added exception handling

--- report.py
+++ report.py
@@ -6,9 +6,22 @@ import psycopg2 as psycopg2
 DATABASE_NAME = 'news'
 
 
+def get_query_results(query):
+    try:
+        connection = psycopg2.connect(database=DATABASE_NAME)
+    except psycopg2.Error as error:
+        print('\n Cannot connect to the "%s" database!\n\n Error - %s'
+              % (DATABASE_NAME, error))
+        sys.exit(1)
+    else:
+        cursor = connection.cursor()
+        cursor.execute(query)
+        result = cursor.fetchall()
+        connection.close()
+        return result
+
+
 def popular_articles():
-    connection = psycopg2.connect(database=DATABASE_NAME)
-    cursor = connection.cursor()
     query = '''
     SELECT
       articles.id,
@@ -25,15 +38,10 @@ def popular_articles():
     ORDER BY views DESC
     LIMIT 3
     '''
-    cursor.execute(query=query)
-    result = cursor.fetchall()
-    connection.close()
-    return result
+    return get_query_results(query)
 
 
 def popular_article_authors():
-    connection = psycopg2.connect(database=DATABASE_NAME)
-    cursor = connection.cursor()
     query = '''
     SELECT
       authors.id,
@@ -52,15 +60,10 @@ def popular_article_authors():
     ORDER BY views DESC
     LIMIT 4
     '''
-    cursor.execute(query=query)
-    result = cursor.fetchall()
-    connection.close()
-    return result
+    return get_query_results(query)
 
 
-def day_requests_error_percentage():
-    connection = psycopg2.connect(database=DATABASE_NAME)
-    cursor = connection.cursor()
+def relevant_day_error_percentages():
     query = '''
     SELECT
         to_char(day, 'Month DD, YYYY'),
@@ -70,51 +73,64 @@ def day_requests_error_percentage():
     WHERE
       (error_count * 100.0 / total_count) > 1.0
     '''
-    cursor.execute(query=query)
-    result = cursor.fetchall()
-    connection.close()
-    return result
+    return get_query_results(query)
+
+
+def print_popular_articles(articles):
+    print('\n The most popular three articles of all time are:')
+    count = 1
+    for article in articles:
+        print('\n %d. "%s" - %d' % (count, article[1], article[2]))
+        count = count + 1
+    return count - 1
+
+
+def print_popular_article_authors(article_authors):
+    print('\n The most popular article authors of all time are:')
+    count = 1
+    for article_author in article_authors:
+        print('\n %d. "%s" - %d' % (count, article_author[1],
+                                    article_author[2]))
+        count = count + 1
+    return count - 1
+
+
+def print_day_requests_error_percentage(day_error_percentages):
+    print('\n The days with more than 1% of requests  leading to error are:')
+    count = 1
+    for day_error_percentage in day_error_percentages:
+        print('\n %d. %s - %.1f%% errors'
+              % (count, day_error_percentage[0], day_error_percentage[1]))
+        count = count + 1
+    return count - 1
+
+
+def print_actions():
+    print('\n Invalid action!\n\n Actions which can be performed are:'
+          '\n\n 1 - Compute the most popular three articles of all time'
+          '\n\n 2 - Compute the most popular article authors of all time'
+          '\n\n 3 - Compute the days with more than 1% of requests'
+          ' leading to error'
+          '\n\n NOTE: Before using this module execute the "views.sql"'
+          ' in your copy of "news" database to create required views.\n')
+
+
+def perform_action(number):
+    print('\n Computing...')
+    count = 0
+    if number == '1':
+        count = print_popular_articles(popular_articles())
+    elif number == '2':
+        count = print_popular_article_authors(popular_article_authors())
+    elif number == '3':
+        count = print_day_requests_error_percentage(
+            relevant_day_error_percentages())
+    print('\n Done printing %d results.\n' % count)
 
 
 if __name__ == '__main__':
     action = sys.argv[1]
-    if action == '1':
-        print('\n Computing the most popular three articles of all time...')
-        articles = popular_articles()
-        count = 1
-        for article in articles:
-            print('\n %d. "%s" - %d' % (count, article[1], article[2]))
-            count = count + 1
-
-        print('\n Done printing %d results.\n' % (count - 1))
-
-    elif action == '2':
-        print('\n Computing the most popular article authors of all time...')
-        article_authors = popular_article_authors()
-        count = 1
-        for article_author in article_authors:
-            print('\n %d. "%s" - %d' % (count, article_author[1],
-                                        article_author[2]))
-            count = count + 1
-
-        print('\n Done printing %d results.\n' % (count - 1))
-
-    elif action == '3':
-        print('\n Computing the days with more than 1% of requests'
-              ' leading to error...')
-        days = day_requests_error_percentage()
-        count = 1
-        for day in days:
-            print('\n %d. %s - %.1f%% errors' % (count, day[0], day[1]))
-            count = count + 1
-
-        print('\n Done printing %d results.\n' % (count - 1))
-
+    if '1' <= action <= '3':
+        perform_action(action)
     else:
-        print('\n Invalid action!\n\n Actions which can be performed are:'
-              '\n\n 1 - Compute the most popular three articles of all time'
-              '\n\n 2 - Compute the most popular article authors of all time'
-              '\n\n 3 - Compute the days with more than 1% of requests'
-              ' leading to error'
-              '\n\n NOTE: Before using this module execute the "views.sql"'
-              ' in your copy of "news" database to create required views.\n')
+        print_actions()
